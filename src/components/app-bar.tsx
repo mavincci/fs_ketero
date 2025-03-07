f57402c92306bdf382242ec4756b95d51a7ddf81
Michael Abebe <notthismichael@gmail.com>
2025-03-07 18:24:07 +0300

    Account name

--- src/components/app-bar.tsx
+++ src/components/app-bar.tsx
@@ -6,7 +6,10 @@ export const AppBar = () => {
   return (
     <div className="flex flex-row justify-between items-center px-8 py-2 border border-b-2">
       <SidebarTrigger />
-      <ThemeSwitcher />
+      <div className='flex items-center gap-8'>
+        <span className='italic'>Michael Abebe</span>
+        <ThemeSwitcher />
+      </div>
     </div>
   );
 };
