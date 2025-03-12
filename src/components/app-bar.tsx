228825ec1fce79a29543b270ff724ce274738571
Michael Abebe <notthismichael@gmail.com>
2025-03-12 11:25:54 +0300

    Added toggle button for on appbar

--- src/components/app-bar.tsx
+++ src/components/app-bar.tsx
@@ -1,11 +1,35 @@
+'use client';
+
 import { ThemeSwitcher } from '@/providers/themeing';
 import React from 'react';
-import { SidebarTrigger } from './ui/sidebar';
+import { useSidebar } from './ui/sidebar';
+import { Button } from './ui/button';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
 
 export const AppBar = () => {
+  const sidebar = useSidebar();
+
   return (
     <div className="flex flex-row justify-between items-center px-8 py-2 border border-b-2">
-      <SidebarTrigger />
+      <span className="space-x-8">
+        <Button
+          data-sidebar="trigger"
+          data-slot="sidebar-trigger"
+          variant="ghost"
+          size="icon"
+          className={'h-7 w-7'}
+          onClick={(event) => {
+            sidebar.toggleSidebar();
+          }}
+        >
+          {sidebar.state == 'expanded' ? <ArrowLeft /> : <ArrowRight />}
+          <span className="sr-only">Toggle Sidebar</span>
+        </Button>
+        {sidebar.state == 'collapsed' && (
+          <span className="font-black text-xl text-[var(--primary)] font-mono">KETERO</span>
+        )}
+      </span>
+
       <div className="flex items-center gap-8">
         <span className="italic">Michael Abebe</span>
         <ThemeSwitcher />
